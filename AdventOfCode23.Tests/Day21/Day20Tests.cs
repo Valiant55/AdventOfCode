d1f7162fa99481de762d1e6931164c3565487c23
Valiant55 <matthew.g.stoltz@gmail.com>
2023-12-20 10:14:25 -0500

    Day 20 prep

---
+++ AdventOfCode23.Tests/Day21/Day20Tests.cs
@@ -0,0 +1,29 @@
+﻿using AdventOfCode23.Core.Day21;
+using FluentAssertions;
+
+namespace AdventOfCode23.Tests.Day21;
+
+public class Day21Tests
+{
+    [Fact]
+    public void Part01_ReturnsExpectedValue_FromExamples()
+    {
+        var parser = new Parser();
+        var solution = new Solution(parser);
+
+        var answer = solution.Part01();
+
+        answer.Should().Be(0);
+    }
+
+    [Fact]
+    public void Part02_ReturnsExpectedValue_FromExamples()
+    {
+        var parser = new Parser();
+        var solution = new Solution(parser);
+
+        var answer = solution.Part02();
+
+        answer.Should().Be(0);
+    }
+}
